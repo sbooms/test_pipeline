94a4f661f85c59e90466ccfdf36e4ab6b7df24cd
sbooms <sbooms@lgcns.com>
2018-09-04 16:00:06 +0900

    123

--- tomcat-helloworld/src/main/java/org/example/HelloServlet.java
+++ tomcat-helloworld/src/main/java/org/example/HelloServlet.java
@@ -12,6 +12,6 @@ public class HelloServlet extends HttpServlet
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, IOException
     {
-        resp.getWriter().print("Hello world");
+        resp.getWriter().print("Hello world 222");
     }
 }
