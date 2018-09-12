7930f255ecb97a5fd58e74fbb9c6e73ba3919e6a
unknown <corp\R11261>
2018-09-12 12:27:53 +0900

    dd

--- todolist/todolist-jdbc-oracle/src/main/java/org/openshift/quickstarts/todolist/dao/JdbcTodoListDAO.java
+++ todolist/todolist-jdbc-oracle/src/main/java/org/openshift/quickstarts/todolist/dao/JdbcTodoListDAO.java
@@ -56,7 +56,6 @@ public class JdbcTodoListDAO implements TodoListDAO {
                 connection.close();
             }
         } catch (SQLException e) {
-	    connection.close();
         }
     }
 
